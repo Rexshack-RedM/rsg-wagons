4c194059c98859bbe91f72bad2dc24a67eddc07b
RexShack <rexshackuk@gmail.com>
2026-09-20 16:07:16 +0100

    Initial commit

---
+++ fxmanifest.lua
@@ -0,0 +1,38 @@
+fx_version 'cerulean'
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+game 'rdr3'
+
+description 'rsg-wagons'
+version '2.0.0'
+
+shared_scripts {
+    '@ox_lib/init.lua',
+    'shared/config.lua'
+}
+
+client_scripts {
+    'client/*.lua'
+}
+
+server_scripts {
+    '@oxmysql/lib/MySQL.lua',
+    'server/server.lua',
+    'server/chop.lua',
+    'server/versionchecker.lua'
+}
+
+dependencies {
+    'ox_lib',
+}
+
+ui_page 'html/index.html'
+
+files {
+    'locales/*.json',
+    'images/*.png',
+    'html/index.html',
+    'html/style.css',
+    'html/script.js'
+}
+
+lua54 'yes'
